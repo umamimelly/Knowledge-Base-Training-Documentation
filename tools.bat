d8cb6ba2910dc6adc66b35c0e99541541aad7eba
umamimelly <110947707+umamimelly@users.noreply.github.com>
2023-09-28 09:23:49 -0600

    Add files via upload

---
+++ tools.bat
@@ -0,0 +1,43 @@
+@echo off
+:: batch file to run tools from USB drive.
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+
+
+cd "D:\"
+
+start bitwindows.exe
+start Ninite.exe
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d D:\brwallpaper.bmp
+reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /f /t REG_SZ /d 10
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+
+exit
